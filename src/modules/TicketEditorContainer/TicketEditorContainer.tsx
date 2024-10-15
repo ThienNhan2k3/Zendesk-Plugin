9ad35ec3603fad40cde9ebe66073c168f7052193
ThienNhan2k3 <ntnhan21@clc.fitus.edu.vn>
2024-10-16 00:37:13 +0700

    Install tailwindcss

--- src/modules/TicketEditorContainer/TicketEditorContainer.tsx
+++ src/modules/TicketEditorContainer/TicketEditorContainer.tsx
@@ -3,8 +3,8 @@
 export default function TicketEditorContainer() : JSX.Element {
     return (
         <div>
-            <button>Draft res</button>
-            <button>Formalize res</button>
+            <button className="bg-green-200">Draft res</button>
+            <button className="bg-red-100">Formalize res</button>
         </div>
     )
 }
--- src/modules/TicketSidebarContainer/TicketSideContainer.tsx
+++ src/modules/TicketSidebarContainer/TicketSideContainer.tsx
@@ -3,7 +3,7 @@
 export default function TicketSidebarContainer() : JSX.Element {
     return (
         <div>
-            <h2>Hello world</h2>
+            <h2 className="text-blue-300">Hello world</h2>
         </div>
     )
 }
